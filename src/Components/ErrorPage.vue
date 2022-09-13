32512bbe94bb4bdfe0e257088ef1de39ea5bc084
Rahul Hruday <rahulhruday@gmail.com>
2022-09-13 22:46:09 +0530

    Implemented Error scenario

---
+++ src/Components/ErrorPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center">
+    <h3 class="m-5">Error : {{ error }}</h3>
+    <b-button pill variant="info" @click="goToHome">Go To Home Page</b-button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ErrorPage',
+  props: ['error'],
+  methods: {
+    goToHome () {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
